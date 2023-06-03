76b2b0f18d5deb8dab129f37970c822f6d693576
adiz50 <adribox12@gmail.com>
2023-06-03 18:42:45 +0200

    Fix

--- backend/src/main/java/put/poznan/backend/controller/BlockchainController.java
+++ backend/src/main/java/put/poznan/backend/controller/BlockchainController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/api/blockcahin")
+@RequestMapping("/api/blockchain")
 @RequiredArgsConstructor
 public class BlockchainController {
 
